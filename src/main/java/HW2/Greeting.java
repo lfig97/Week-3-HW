40a06e730cc9e211d2209303bb1d62b26fe7526b
Luis Figueroa <lfigueroa002@my.wilmu.edu>
2022-03-30 09:36:00 -0400

    Adding ignoreCase function

--- src/main/java/HW2/Greeting.java
+++ src/main/java/HW2/Greeting.java
@@ -5,32 +5,25 @@ import java.util.Scanner;
 public class Greeting {
     private String enteredName;
 
-    public void askName(){
+    public void askName() {
         Scanner userInput = new Scanner(System.in);
         System.out.println("Enter your name");
         this.enteredName = userInput.nextLine();
         checkName(this.enteredName);
     }
-    public void checkName(String enteredName){
+
+    public void checkName(String enteredName) {
         boolean checkEnteredName = true;
-        label:
-        while(checkEnteredName){
-            switch (this.enteredName) {
-                case "Alice":
-                    System.out.println("Greetings Alice");
-                    break label;
-                case "alice":
-                    System.out.println("Greetings Alice");
-                    break label;
-                case "bob":
-                    System.out.println("Greetings Bob");
-                    break label;
-                case "Bob":
-                    System.out.println("Greetings Bob");
-                    break label;
-                default:
-                    askName();
-                    break label;
+        while (checkEnteredName) {
+            if (this.enteredName.equalsIgnoreCase("Alice")) {
+                System.out.println("Greetings Alice");
+                break;
+            } else if (this.enteredName.equalsIgnoreCase("Bob")) {
+                System.out.println("Greetings Bob");
+                break;
+            } else {
+                askName();
+                break;
             }
         }
     }
